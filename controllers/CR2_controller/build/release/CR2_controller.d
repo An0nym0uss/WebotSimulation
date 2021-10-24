552bec94161f8d61d40ae3b00f9b7ed6581bb6bb
An0nym0uss <77669260+An0nym0uss@users.noreply.github.com>
2021-10-25 08:30:28 +1100

    Add files via upload

---
+++ controllers/CR2_controller/build/release/CR2_controller.d
@@ -0,0 +1,8 @@
+build/release/CR2_controller.o: CR2_controller.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Receiver.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Emitter.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Keyboard.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp
